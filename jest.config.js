08038b484052dc2d16faf43043e4fc9d1a53df63
Yincheng Hou <houyc1217@gmail.com>
2026-01-24 19:32:25 +0000

    Revert "Cleanup and simplify project structure: remove unused assets and redundant configs"
    This reverts commit 55938630da45172a9a0e8fe475c88b7cfa033558.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'js/**/*.js',
+    '!**/node_modules/**'
+  ],
+  verbose: true
+};
